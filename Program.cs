cfcbf66e9d406c275b0c4baeba9837be6bc854b5
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-10 16:31:57 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace structure
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Apples.");
         }
     }
 }
